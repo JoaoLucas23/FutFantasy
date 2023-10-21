a7052b6680cef2e6fd1bd688d1c4004b216eaea2
JoaoLucas23 <jllgoncalves23@gmail.com>
2023-10-20 21:36:32 -0300

    feat: modelo Usuario

---
+++ server/entidades/models/Props.ts
@@ -0,0 +1,9 @@
+export interface UsarioProps {
+    id?: number;
+    nome: string;
+    email: string;
+    senha?: string;
+    saldo?: number;
+}
+
+    
---
+++ server/entidades/models/Usuario.ts
@@ -0,0 +1,43 @@
+import { sequelize } from "../../database";
+import { DataTypes } from "sequelize";
+
+export const Usuario = sequelize.define('Usuario', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: {
+            name: 'email',
+            msg: 'Email já cadastrado'
+        }
+    },
+    senha: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    saldo: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        defaultValue: 0,
+    },
+},
+{
+    timestamps: false,
+}
+);
+
+Usuario.sync({alter: true, force: false})
+    .then(() => {
+        console.log("Tabela de Usuários criada")
+    })
+    .catch((error) => {
+        console.log("Erro ao criar tabela de Usuários: " + error)
+});
