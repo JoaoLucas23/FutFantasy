3af5a97f0721046c54f20835ac718945709ba3c5
JoaoLucas23 <jllgoncalves23@gmail.com>
2023-10-23 17:28:45 -0300

    feat: rotas usuarios

--- server/src/entidades/controllers/RotasUsuario.ts
+++ server/src/entidades/controllers/RotasUsuario.ts
@@ -21,7 +21,78 @@ router.post(('/criarUsuario'),
     async (req, res, next) => {
         try {
             const usuario = await ServicosUsuario.criaUsuario(req.body);
-            res.status(204).json(usuario);
+            res.status(201).json(usuario);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+router.put(('/editarUsuario/:id'),
+    async (req, res, next) => {
+        try {
+            const id = parseInt(req.params.id);
+            const usuario = await ServicosUsuario.editaUsuario(id, req.body);
+            res.status(200).json(usuario);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+router.delete(('/deletarUsuario/:id'),
+    async (req, res, next) => {
+        try {
+            const id = parseInt(req.params.id);
+            await ServicosUsuario.deletaUsuario(id);
+            res.status(200).end();
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+router.get(('/retornaUsuarioPorId/:id'),
+    async (req, res, next) => {
+        try {
+            const id = parseInt(req.params.id);
+            const usuario = await ServicosUsuario.retornaUsuarioPorId(id);
+            res.status(200).json(usuario);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+router.get(('/retornaTodosUsuarios'),
+    async (req, res, next) => {
+        try {
+            const usuarios = await ServicosUsuario.retornaTodosUsuarios();
+            res.status(200).json(usuarios);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+router.get(('/retornaUsuarioPorEmail/:email'),
+    async (req, res, next) => {
+        try {
+            const email = req.params.email;
+            const usuario = await ServicosUsuario.retornaUsuarioPorEmail(email);
+            res.status(200).json(usuario);
+        } catch (error) {
+            next(error);
+        }
+    }
+);
+
+router.get(('/retornaUsuarioPorNome/:nome'),
+    async (req, res, next) => {
+        try {
+            const nome = req.params.nome;
+            const usuario = await ServicosUsuario.retornaUsuarioPorNome(nome);
+            res.status(200).json(usuario);
         } catch (error) {
             next(error);
         }
